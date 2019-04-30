1098f5fe3212e33c5617c7555f62cf77a2c2a4e2
Jesus Alba <jesus.alba9504@gmail.com>
2019-04-30 09:24:29 -0400

    Files generated by PkgTemplates

---
+++ src/KmeansAlgorithmTask.jl
@@ -0,0 +1,5 @@
+module KmeansAlgorithmTask
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using KmeansAlgorithmTask
+using Test
+
+@testset "KmeansAlgorithmTask.jl" begin
+    # Write your own tests here.
+end
